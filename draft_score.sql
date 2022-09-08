8bbd829905452b73bde29e402aa2c84a0d6c3b73
kaymeezy <112824339+kaymeezy@users.noreply.github.com>
2022-09-08 12:22:20 -0400

    Update draft_score.sql

--- draft_score.sql
+++ draft_score.sql
@@ -1,24 +1,36 @@
 SELECT
-*,
-(wkly_fpts / (SELECT MAX(wkly_fpts) FROM fantasy.draft_metrics)) * 0.25 
+dm.*,
+(dm.wkly_fpts / (SELECT MAX(wkly_fpts) FROM fantasy.draft_metrics)) * 0.25 
 +
-(wkly_cnst / (SELECT MAX(wkly_cnst) FROM fantasy.draft_metrics)) * 0.1
+(dm.wkly_cnst / dm.wkly_fpts) * 0.1
 +
-(FPTS / (SELECT MAX(FPTS) FROM fantasy.draft_metrics)) * 0.2
+(dm.FPTS / (SELECT MAX(FPTS) FROM fantasy.draft_metrics)) * 0.2
 +
-(SEASON_CNST / (SELECT MAX(SEASON_CNST) FROM fantasy.draft_metrics)) * 0.1
+(dm.SEASON_CNST / dm.FPTS) * 0.1
 +
-(wkly_gp / (SELECT MAX(wkly_gp) FROM fantasy.draft_metrics)) * 0.1
+(dm.wkly_gp / (SELECT MAX(wkly_gp) FROM fantasy.draft_metrics)) * 0.1
 +
-(GP / (SELECT MAX(GP) FROM fantasy.draft_metrics)) * 0.1
+(dm.GP / (SELECT MAX(GP) FROM fantasy.draft_metrics)) * 0.1
 +
-(PFP * 0.05)
+(dm.PFP * 0.05)
 +
-(1 - ((player_rank-1) / (SELECT MAX(player_rank) FROM fantasy.draft_metrics))) * 0.1
-AS SCORE
+(1 - ((COALESCE(dm.player_rank,346)-1) / (SELECT MAX(player_rank) FROM fantasy.draft_metrics))) * 0.1
+AS SCORE,
+pred.seas_diff,
+pred.wkly_diff
 
-FROM fantasy.draft_metrics
+FROM fantasy.draft_metrics dm
+LEFT JOIN fantasy.pred_diff pred
+ON dm.playerId = pred.playerId
 
 ORDER BY SCORE DESC
 
-LIMIT 100
+LIMIT 200
+
+
+
+
+
+
+
+
